43a5d6cdf9d49d6e3b8afde720f3aef37e6041e3
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-20 07:23:42 -0300

    create-app and first adjustments

---
+++ src/components/Formulario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <h1 className="text-dark">
+            Form here
+        </h1>
+    );
+}
+
+export default Formulario;
