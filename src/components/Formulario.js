60af1ac9eee0b6fbd965041065355669f23254ea
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-20 18:19:54 -0300

    create-form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,9 +2,40 @@ import React from 'react';
 
 const Formulario = () => {
     return (
-        <h1 className="text-dark">
-            Form here
-        </h1>
+        <form
+            className="col-md-12"
+        >
+            <fieldset>
+                <legend>Search drinks by categories or ingredients</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                         name="nombre"
+                         className="form-control"
+                         type="text"
+                         placeholder="Search by ingredient"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">-- Select Category --</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Find Drinks"
+                    />
+                </div>
+            </div>
+        </form>
     );
 }
 
