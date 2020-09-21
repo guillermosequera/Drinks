38a2938e73e4c16db21b9e456fff19229c1503ec
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-21 05:05:08 -0300

    creating a second context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Fragment } from 'react';
 
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+
 import CategoriasProvider from './context/CategoriasContext';
+import RecetasProvider from './context/RecetasContext';
 
 
 function App() {
   return (
       <CategoriasProvider>
+        <RecetasProvider>
           <Header />
 
           <div className="container mt-5">
@@ -15,6 +18,7 @@ function App() {
               <Formulario />
             </div>
           </div>
+        </RecetasProvider>
       </CategoriasProvider>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { CategoriasContext } from '../context/CategoriasContext';
+import { RecetasContext } from '../context/RecetasContext';
 
 const Formulario = () => {
 
@@ -9,6 +10,7 @@ const Formulario = () => {
     });
 
     const { categorias } = useContext(CategoriasContext);
+    const { buscarRecetas } = useContext(RecetasContext);
 
     //funcion para leer los contenidos
     const obtenerDatosReceta = e => {
@@ -21,6 +23,10 @@ const Formulario = () => {
     return (
         <form
             className="col-md-12"
+            onSubmit={e => {
+                e.preventDefault();
+                buscarRecetas(busqueda)
+            }}
         >
             <fieldset>
                 <legend>Search drinks by categories or ingredients</legend>
